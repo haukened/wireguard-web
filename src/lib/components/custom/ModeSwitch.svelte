80aa702b3c4e284000fec1be311ca16ef803829d
David Haukeness <david@hauken.us>
2024-10-30 14:32:40 +0000

    Refactor ModeSwitch component to use localized labels for theme modes

--- src/lib/components/custom/ModeSwitch.svelte
+++ src/lib/components/custom/ModeSwitch.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import Sun from "lucide-svelte/icons/sun";
     import Moon from "lucide-svelte/icons/moon";
+    import * as m from "$lib/paraglide/messages";
     
     import { resetMode, setMode } from "mode-watcher";
     import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
@@ -16,14 +17,12 @@
     <Moon
     class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
     />
-    <span class="sr-only">Toggle theme</span>
+    <span class="sr-only">{m.modeDescription()}</span>
     </Button>
 </DropdownMenu.Trigger>
 <DropdownMenu.Content align="end">
-    <DropdownMenu.Item on:click={() => setMode("light")}
-    >Light</DropdownMenu.Item
-    >
-    <DropdownMenu.Item on:click={() => setMode("dark")}>Dark</DropdownMenu.Item>
-    <DropdownMenu.Item on:click={() => resetMode()}>System</DropdownMenu.Item>
+    <DropdownMenu.Item on:click={() => setMode("light")}>{m.lightModeLabel()}</DropdownMenu.Item>
+    <DropdownMenu.Item on:click={() => setMode("dark")}>{m.darkModeLabel()}</DropdownMenu.Item>
+    <DropdownMenu.Item on:click={() => resetMode()}>{m.systemModeLabel()}</DropdownMenu.Item>
 </DropdownMenu.Content>
 </DropdownMenu.Root>
