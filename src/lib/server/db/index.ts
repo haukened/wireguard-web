e49c3f3ddf1d18d1ae9805559db8ac1c2c6135ff
David Haukeness <david@hauken.us>
2024-10-30 21:09:20 +0000

    create user schema

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,5 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
 import { env } from '$env/dynamic/private';
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = new Database(env.DATABASE_URL);
+const client = new Database( env.DB_URL || 'db.sqlite' );
 export const db = drizzle(client);
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,12 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { sql } from 'drizzle-orm';
 
-export const user = sqliteTable('user', {
-	id: integer('id').primaryKey(),
-	age: integer('age')
+export const users = sqliteTable('users', {
+	id: integer('id', {mode: 'number'}).primaryKey({autoIncrement: true}),
+	username: text('username').notNull(),
+	password: text('password').notNull(),
+	disabled: integer('disabled', {mode: 'boolean'}).notNull().default(false),
+	created_at: integer('created_at', {mode: 'timestamp'}).notNull().default(sql`CURRENT_TIMESTAMP`),
+	updated_at: integer('updated_at', {mode: 'timestamp'}).notNull().default(sql`CURRENT_TIMESTAMP`),
+	last_login: integer('last_login', {mode: 'timestamp'}),
 });
