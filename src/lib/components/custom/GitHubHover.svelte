b6b255bafe5772ead31beaf87cf292f0cbe8ffda
David Haukeness <david@hauken.us>
2024-10-30 14:20:22 +0000

    Refactor GitHubHover component to remove redundant skeleton loading element

--- src/lib/components/custom/GitHubHover.svelte
+++ src/lib/components/custom/GitHubHover.svelte
@@ -30,11 +30,11 @@
     <HoverCard.Content class="w-80">
     {#await dataPromise}
     <div class="flex items-center space-x-4">
-      <Skeleton class="h-12 w-12 rounded-full" />
       <div class="space-y-2 w-full">
         <Skeleton class="h-4 w-full" />
         <Skeleton class="h-4 w-full" />
       </div>
+      <Skeleton class="h-12 w-12 rounded-full" />
     </div>
     {:then data}
     <div class="flex flex-col space-y-4">
